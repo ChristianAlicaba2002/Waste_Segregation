b2f5e5c7cc6412a42c20fe94afc55b0bee35dcf6
quivus <v.rajime.v@gmail.com>
2025-04-27 03:43:40 +0800

    Binnie ID Added

--- resources/views/ClientSide/Auth/Register.blade.php
+++ resources/views/ClientSide/Auth/Register.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <link rel="icon" type="image/png" href="img/logo.png">
+    <link rel="icon" type="image/png" href="img/IM_logo.png">
     <link rel="stylesheet" href="Styles/Register.css">
     <title>Segregation Trash Bin Register</title>
 </head>
@@ -19,23 +19,19 @@
         <div class="left-section">
             <img src="{{ asset('img/trash.gif') }}">
             <h2>Segregation Mission</h2>
-            <h2>Join Now!</h2>
+            <p>"Smart Sorting, Sustainable Living—Automating Waste for a Greener Future." 🌍♻️</p>
         </div>
         <div class="register-container">
             <h1>Create an Account</h1>
             <form action="{{route('registerClient')}}" method="POST" id="SubmitForm">
                 @csrf
                 <div class="form-group">
-                    <label for="first_name">First Name</label>
-                    <input type="text" id="first_name" name="first_name" required placeholder="Enter your first name" required>
+                    <label for="binnie_id"></label>
+                    <input type="text" id="binnie_id" name="binnie_id" required placeholder="Binnie ID" required>
                 </div>
                 <div class="form-group">
-                    <label for="last_name">Last Name</label>
-                    <input type="text" id="last_name" name="last_name" required placeholder="Enter your last name" required>
-                </div>
-                <div class="form-group">
-                    <label for="username">Username</label>
-                    <input type="text" id="username" name="username" required placeholder="Enter your username" required>
+                    <label for="username"></label>
+                    <input type="text" id="username" name="username" required placeholder="Username" required>
                     @if (session('usernameExists'))
                         <label class="ErrorMessage" for="">{{ session('usernameExists') }}</label>
                         <script>
@@ -44,8 +40,16 @@
                     @endif
                 </div>
                 <div class="form-group">
-    <label for="password">Password</label>
-    <input type="password" id="password" name="password" required placeholder="Enter your password" autocomplete="off">
+                    <label for="first_name"></label>
+                    <input type="text" id="first_name" name="first_name" required placeholder="First Name" required>
+                </div>
+                <div class="form-group">
+                    <label for="last_name"></label>
+                    <input type="text" id="last_name" name="last_name" required placeholder="Last Name" required>
+                </div>
+                <div class="form-group">
+    <label for="password"></label>
+    <input type="password" id="password" name="password" required placeholder="Password" autocomplete="off">
     <i class="fa fa-eye toggle-icon" id="togglePassword"></i>
     @if(session('passlengthrequired'))
         <label class="ErrorMessage" for="">{{ session('passlengthrequired') }}</label>
@@ -56,8 +60,8 @@
 </div>
 
 <div class="form-group">
-    <label for="confirm_password">Confirm Password</label>
-    <input type="password" id="confirm_password" name="confirm_password" required placeholder="Confirm your password">
+    <label for="confirm_password"></label>
+    <input type="password" id="confirm_password" name="confirm_password" required placeholder="Confirm Password">
     <i class="fa fa-eye toggle-icon" id="toggleConfirmPassword"></i>
     @if(session('passwordMismatch'))
         <label class="ErrorMessage" for="">{{ session('passwordMismatch') }}</label>
